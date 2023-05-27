81d948ad1715036524bb130cacea9d02ff53a832
Albert Hidalgo <alhidalgodev@gmail.com>
2023-05-26 23:28:11 -0400

    feat
    * Added contact form funtionality

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,10 +1,50 @@
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
 import config from "@config/config.json";
+import Spinner from './components/Spinner';
 import { markdownify } from "@lib/utils/textConverter";
 
 const Contact = ({ data }) => {
   const { frontmatter } = data;
   const { title, info } = frontmatter;
   const { contact_form_action } = config.params;
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [subject, setSubject] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // Perform form submission logic here (e.g., send data to backend)
+    const templateParams = {
+      name,
+      email,
+      subject,
+      message
+    };
+
+    emailjs.send('service_22tltyn', 'template_wt3snog', templateParams, '-qFHvZCxvKML4Pcwg')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setSuccess(true);
+      }, (err) => {
+        setSuccess(true);
+        console.log('FAILED...', err);
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+
+    // Reset form fields
+    setName('');
+    setEmail('');
+    setSubject('');
+    setMessage('');
+  };
 
   return (
     <section className="section">
@@ -14,8 +54,7 @@ const Contact = ({ data }) => {
           <div className="col-12 md:col-6 lg:col-7">
             <form
               className="contact-form"
-              method="POST"
-              action={contact_form_action}
+              onSubmit={handleSubmit}
             >
               <div className="mb-3">
                 <input
@@ -23,6 +62,8 @@ const Contact = ({ data }) => {
                   name="name"
                   type="text"
                   placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   required
                 />
               </div>
@@ -32,6 +73,8 @@ const Contact = ({ data }) => {
                   name="email"
                   type="email"
                   placeholder="Your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                 />
               </div>
@@ -41,6 +84,8 @@ const Contact = ({ data }) => {
                   name="subject"
                   type="text"
                   placeholder="Subject"
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
                   required
                 />
               </div>
@@ -49,11 +94,22 @@ const Contact = ({ data }) => {
                   className="form-textarea w-full rounded-md"
                   rows="7"
                   placeholder="Your message"
+                  name="message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 />
               </div>
-              <button type="submit" className="btn btn-primary">
-                Send Now
-              </button>
+              {success ? (
+                <div className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                  <span className="font-medium">Thank you!</span> Your message has been submitted successfully 🎉. We will get back to you soon. 😊
+                </div>
+
+              ) : (
+                <button type="submit" className="btn btn-primary">
+                  {loading ? <Spinner /> : 'Send Now'}
+                </button>
+              )
+              }
             </form>
           </div>
           <div className="content col-12 md:col-6 lg:col-5">
